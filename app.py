bf503326c32c7bb1818d9635d9f83ce8b5334f32
James Andrew Burnett <andyburnett@Mac.attlocal.net>
2025-04-23 06:35:36 -0500

    Add .gitignore file

--- app.py
+++
@@ -1,5 +0,0 @@
-from fastapi import FastAPI
-from api import user_routes
-
-app = FastAPI()
-app.include_router(user_routes.router)
--- models/user_model.py
+++
@@ -1,8 +0,0 @@
-from pydantic import BaseModel
-from datetime import datetime
-
-class User(BaseModel):
-    name: str
-    email: str
-    certified: bool
-    login_time: datetime
